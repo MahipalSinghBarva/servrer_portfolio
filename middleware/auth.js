5985b9f89b381e2c1ebc1fbf2f1a55ca49498c3d
Mahipal Singh <mahipalsingh450@gmail.com>
2024-12-10 16:04:40 +0530

    fd

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,13 +8,20 @@ dotenv.config();
 
 exports.isAuthenticated = catchAsyncError(async (req, res, next) => {
     const { token } = req.cookies;
+
     if (!token) {
         return next(new ErrorHandler("User not authenticated", 401));
     }
     console.log("Token from cookies:", token);
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = await User.findById(decoded.id);
+        req.user = await User.findById(decoded._id);
+        console.log("user auth", req.user);
+
+        if (!req.user) {
+            return next(new ErrorHandler("User not authenticated", 401));
+        }
         next();
     } catch (err) {
         return next(new ErrorHandler("Invalid or expired token", 401));
